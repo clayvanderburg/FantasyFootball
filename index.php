237e8a55ad15b70b0ff74d40b3bc378e25580905
Clay Vanderburg <clayvanderburg@gmail.com>
2017-11-20 17:43:23 -0600

    cleaned up excess line breaks

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
 	include('includes/header.php');
-
 	//Scrape from fantasy pros
 	$fantasyPros = file_get_contents('https://www.fantasypros.com/nfl/cheatsheets/top-players.php');
-
 	//parse out unnecessary stuff
 	$pageArray = explode('.&nbsp;', $fantasyPros);
-
 	$pageArray1 = preg_replace('/\d+/u','',$pageArray);
 	$pageArray1 = preg_replace( "/\r|\n/", "", $pageArray1 );
-
-    $playersAll = array_slice($pageArray1,1,330);
-
-    $playersAll = str_replace(array('</ul></div><div class="four columns"><ul>','</li><li class="mpb-player-">'),array('',''), $playersAll);
-
-
-    // $playersAll = str_replace(,'', $playersAll[2]);
-
+  $playersAll = array_slice($pageArray1,1,330);
+	$playersAll = str_replace(array('</ul></div><div class="four columns"><ul>','</li><li class="mpb-player-">'),array('',''), $playersAll);
 ?>
-
 		<div class="modal fade" id="instructionModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
 		  <div class="modal-dialog" role="document">
 		    <div class="modal-content">
 		      <div class="modal-header text-center">
 		        <h5 class="modal-title" id="exampleModalLabel">Welcome to FFBCheatSheet.info!</h5>
-
 		      </div>
 		      <div class="modal-body text-center">
 		        <p>This site is intended to be a replacement for a physical cheat sheet at live fantasy drafts.</p>
@@ -35,31 +24,25 @@
 		      </div>
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
 		      </div>
 		    </div>
 		  </div>
 		</div>
    		<div class="container-fluid" >
 		    <div class="row">
-
 		    	<div class="col-xs-12 rowpadsmall">
 		    		<ul id="parent" class="list1">
 				    	<?php foreach( $playersAll as $playersAll): ?>
 				    		<li class="listitem1 parent" draggable="true"><?= $playersAll?><span class="handle"><i class="fa fa-sort-asc" aria-hidden="true"></i>
 									<i class="fa fa-sort-desc" aria-hidden="true"></i>
-
-</span></li>
+								</span></li>
 				    	<?php endforeach; ?>
 			    	</ul>
 		    	</div>
 		    </div>
 		</div>
 		<script type="text/javascript">$(".listitem1 a").removeAttr("href");</script>
-
 <?php
-
-//link to footer
-include('includes/footer.php');
-
+	//link to footer
+	include('includes/footer.php');
 ?>
