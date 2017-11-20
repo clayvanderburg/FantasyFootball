f993d0a87d95d9ca6106161ae33e1c74fa2a5594
Clay Vanderburg <clayvanderburg@gmail.com>
2017-11-20 17:45:55 -0600

    cleaned up excess line breaks

--- ppr.php
+++ ppr.php
@@ -1,39 +1,26 @@
 <?php
 	include('includes/header.php');
-
 	$fantasyPros = file_get_contents('https://www.fantasypros.com/nfl/cheatsheets/top-ppr-players.php');
-
-    //parse out unnecessary stuff
+  //parse out unnecessary stuff
 	$pageArray = explode('.&nbsp;', $fantasyPros);
-
 	$pageArray1 = preg_replace('/\d+/u','',$pageArray);
 	$pageArray1 = preg_replace( "/\r|\n/", "", $pageArray1 );
-
-    $playersAll = array_slice($pageArray1,1,330);
-
-    $playersAll = str_replace(array('</ul></div><div class="four columns"><ul>','</li><li class="mpb-player-">'),array('',''), $playersAll);
-
+  $playersAll = array_slice($pageArray1,1,330);
+  $playersAll = str_replace(array('</ul></div><div class="four columns"><ul>','</li><li class="mpb-player-">'),array('',''), $playersAll);
 ?>
-
-
-
    		<div class="container-fluid" >
 		    <div class="row">
-		    		<!-- RANKED PLAYER LIST -->
+		    	<!-- RANKED PLAYER LIST -->
 		    	<div class="col-xs-12 rowpadsmall">
 		    		<ul id="parent-ppr"class="list1 ">
 				    	<?php foreach( $playersAll as $playersAll ): ?>
 								<li class="listitem1 parent-ppr" draggable="true"><?= $playersAll?><span class="handle"><i class="fa fa-sort-asc" aria-hidden="true"></i>
-									<i class="fa fa-sort-desc" aria-hidden="true"></i>
-
+								<i class="fa fa-sort-desc" aria-hidden="true"></i></span></li>
 				    	<?php endforeach; ?>
 			    	</ul>
 		    	</div>
 		</div>
-
 	<?php
-
 		//link to footer
 		include('includes/footer.php');
-
 	?>
